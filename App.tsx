a4dbb6315c404c9de94460190a2b9c31316e1bfa
Phat Duong <beebapcay@gmail.com>
2021-07-15 12:02:33 +0700

    Setup ReactNavigation

--- App.tsx
+++ App.tsx
@@ -1,11 +1,22 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import { NativeBaseProvider, Box, StatusBar } from 'native-base';
+import { NavigationContainer } from '@react-navigation/native';
 
-export default function App() {
+/**
+ * Root component in project
+ *
+ * @returns JSXElement
+ */
+const App = () => {
   return (
-    <NativeBaseProvider>
-      <StatusBar />
-      <Box>Hello World!!!</Box>
-    </NativeBaseProvider>
+    <NavigationContainer>
+      <NativeBaseProvider>
+        <StatusBar />
+        <Box>Hello World!!!</Box>
+      </NativeBaseProvider>
+    </NavigationContainer>
   );
-}
+};
+
+export default App;
